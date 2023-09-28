1c447e15321194cbe4139608881cb7ad7059fc71
Hanaline <gaelle.boutolini.simplon@gmail.com>
2023-09-28 15:09:04 +0200

    Ajout des fichiers: style.css, script.js, preload.js

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
 console.log('Hello from Electron 👋')
-const { app, BrowserWindow } = require('electron');
+// main.js
 
-function createWindow() {
-  // Créez une fenêtre de navigateur Electron
+// Modules pour la gestion de l'appli et la création de la BrowserWindow native browser window
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+  // Création de la browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true, // Active l'intégration de Node.js dans la fenêtre
-    },
-  });
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
-  // Chargez un fichier HTML dans la fenêtre
-  mainWindow.loadFile('index.html');
+  // et chargement de l'index.html de l'application.
+  mainWindow.loadFile('index.html')
+
+  // Ouvrir les outils de développement.
+  // mainWindow.webContents.openDevTools()
 }
 
-// Événement déclenché lorsque l'application est prête
-app.whenReady().then(createWindow);
+// Cette méthode sera appelée quand Electron aura fini
+// de s'initialiser et sera prêt à créer des fenêtres de navigation.
+// Certaines APIs peuvent être utilisées uniquement quant cet événement est émit.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    // Sur macOS il est commun de re-créer une fenêtre  lors 
+    // du click sur l'icone du dock et qu'il n'y a pas d'autre fenêtre ouverte.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quitter quand toutes les fenêtres sont fermées, sauf sur macOS. Dans ce cas il est courant
+// que les applications et barre de menu restents actives jusqu'à ce que l'utilisateur quitte 
+// de manière explicite par Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// Dans ce fichier vous pouvez inclure le reste du code spécifique au processus principal. Vous pouvez également le mettre dans des fichiers séparés et les inclure ici.
+
