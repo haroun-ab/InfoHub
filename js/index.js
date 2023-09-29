34a746bf5feb99d074b510c585c0294d5f18a415
Haroun <hrnabd@hotmail.com>
2023-09-29 12:17:45 +0200

    connection with API

--- js/index.js
+++ js/index.js
@@ -1,6 +1,67 @@
-const loader = document.querySelector('.loader')
+// Disparition loader
+const loader = document.querySelector('.loader');
+
+
+
 
 setTimeout(() => {
     loader.remove();
-}, 5000);
+    getapi();
+}, 3000);
+
+
+/////////////////////////////////////////////////
+async function getapi() {
+    try {
+        const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent("https://newsapi.org/v2/top-headlines?country=us&apiKey=6147950b50674ba4a220b9a00c9cd1f0")}`, {
+          method: 'GET',
+          
+        });
+        if (!response.ok) {
+            console.log(response)
+
+          throw new Error('Network response was not ok');
+        }
+        else {
+            const contentResponse = await response.json();
+            const articleList = JSON.parse(contentResponse.contents).articles;
+    console.log(articleList[1]);
+            articleList.forEach(content => {
+                document.querySelector('main').innerHTML += `<article>
+                
+                <img src="${content.urlToImage}">
+                    <div>
+                    <p>${content.title}</p>
+                    <p>${content.source.name}</p>
+                    </div>
+                </article>`;
+            });
+
+            
+        }
+        
+      } catch (error) {
+        console.error('Erreur lors de la requête fetch :', error);
+      }
+
+
+// const url = 'https://google-news13.p.rapidapi.com/latest?lr=fr-FR';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '59edeafd1emshaff249be04e1192p1fafb0jsnd0659e797c1c',
+// 		'X-RapidAPI-Host': 'google-news13.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.json();
+// 	console.log(result);
+//     document.querySelector('main').innerHTML = result.items
+// } catch (error) {
+// 	console.error(error);
+// }
+
+}
 
--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-
 const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    transparent: true, 
-    // focusable: true
-    // backgroundMaterial: 'acrylic',
-  })
+    webPreferences: {
+      nodeIntegration: true, 
+      contextIsolation: false, 
+    },
+  });
 
   win.loadFile('index.html')
 }
@@ -28,3 +28,4 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
